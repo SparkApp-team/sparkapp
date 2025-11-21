7e859e927bc803387c30b62a8f21ad9a317cb5f2
Anjali SIngh <anj7944@gmail.com>
2025-11-22 02:53:10 +0530

    Initialised spring boot project in backend/

---
+++ backend/sparkAPI/src/main/java/com/sparkApp/sparkAPI/SparkApiApplication.java
@@ -0,0 +1,13 @@
+package com.sparkApp.sparkAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparkApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SparkApiApplication.class, args);
+	}
+
+}
---
+++ backend/sparkAPI/src/test/java/com/sparkApp/sparkAPI/SparkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparkApp.sparkAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SparkApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
