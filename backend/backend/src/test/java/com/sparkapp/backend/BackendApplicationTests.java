d0a483fc962e7f00181bd9127c23c072b385eaf8
unknown <radionroman@gmail.com>
2025-11-22 19:25:48 +0100

    Initial sptring boot setup

---
+++ backend/backend/src/test/java/com/sparkapp/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparkapp.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
