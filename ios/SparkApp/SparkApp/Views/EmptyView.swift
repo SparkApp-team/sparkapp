1c111f595da6c82e0a981649d99e221fa9e9d8b5
Dima Kryzhanovsky <dimakryzha@gmail.com>
2025-11-24 00:02:51 +0200

    Add LoginView, SplashView, LaunchScreen.

---
+++ ios/SparkApp/SparkApp/Views/EmptyView.swift
@@ -0,0 +1,16 @@
+//
+//  EmptyView.swift
+//  SparkApp
+//
+//  Created by Dmitro Kryzhanovsky on 23.11.2025.
+//
+
+import SwiftUI
+
+struct EmptyView: View {
+    var body: some View {
+        Text("You are logged in!")
+            .font(.largeTitle)
+            .padding()
+    }
+}
