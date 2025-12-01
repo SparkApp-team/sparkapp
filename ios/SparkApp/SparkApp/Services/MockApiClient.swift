30028af65b385b7f6588c3509c54229147e7fa44
Dima Kryzhanovsky <dimakryzha@gmail.com>
2025-12-01 18:15:17 +0200

    Change 'secondaryBackground' color.

--- ios/SparkApp/SparkApp/Services/MockApiClient.swift
+++ ios/SparkApp/SparkApp/Services/MockApiClient.swift
@@ -17,9 +17,9 @@ final class MockApiClient {
         try await Task.sleep(for: .seconds(0.8))
         
         return [
-            Habit(id: "1", name: "Push Ups", type: .counter, createdAt: Date(), updatedAt: Date()),
-            Habit(id: "2", name: "Squating", type: .counter, createdAt: Date(), updatedAt: Date()),
-            Habit(id: "3", name: "Clean room", type: .boolean, createdAt: Date(), updatedAt: Date())
+            Habit(id: UUID().uuidString, name: "Push Ups", type: .counter, createdAt: Date(), updatedAt: Date()),
+            Habit(id: UUID().uuidString, name: "Squating", type: .counter, createdAt: Date(), updatedAt: Date()),
+            Habit(id: UUID().uuidString, name: "Clean room", type: .boolean, createdAt: Date(), updatedAt: Date())
         ]
     }
 }
--- ios/SparkApp/SparkApp/Views/Habit/HabitListView.swift
+++ ios/SparkApp/SparkApp/Views/Habit/HabitListView.swift
@@ -9,11 +9,11 @@ import SwiftUI
 
 //Fix:
 //1. normal loading animation + background
+//2. think about secondary background color
+//3. border for list
 
 struct HabitListView: View {
     @StateObject private var viewModel = HabitViewModel()
-    
-    var titles = ["First Section" : ["Manage your workout", "View recorded workouts", "Weight tracker", "Mediation"], "Second Section" : ["Your workout", "Recorded workouts", "Tracker", "Mediations"]]
     
     init() {
         UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor: AppColors.P2.textPrimary.uiColor]
@@ -34,34 +34,18 @@ struct HabitListView: View {
                     .frame(maxWidth: .infinity, maxHeight: .infinity)
                 } else {
                     List {
-                        VStack(spacing: 0) {
-                            ForEach(viewModel.habits) { habit in
-                                HStack {
-                                    Text(habit.name)
-                                        .foregroundColor(AppColors.P2.textPrimary)
-                                    Spacer()
-                                }
-                                .padding()
-                                
-                                if habit.id != viewModel.habits.last?.id {
-                                    Divider()
-                                        .background(AppColors.P2.textPrimary)
-                                }
+                        ForEach(viewModel.habits) { habit in
+                            HStack {
+                                Text(habit.name)
+                                    .foregroundColor(AppColors.P2.textPrimary)
+                                Spacer()
                             }
+                            .listRowBackground(
+                                AppColors.P2.secondaryBackground
+                            )
                         }
-                        .background(
-                            RoundedRectangle(cornerRadius: 12)
-                                .fill(AppColors.P2.secondaryBackground)
-                                .overlay(
-                                    RoundedRectangle(cornerRadius: 12)
-                                        .stroke(AppColors.P2.textPrimary, lineWidth: 1)
-                                )
-                        )
-                        .listRowBackground(Color.clear)
-                        .padding(.vertical, 4)
                     }
                     .scrollContentBackground(.hidden)
-                    .background(AppColors.P2.background)
                     .listStyle(.insetGrouped)
                 }
             }
